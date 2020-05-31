bac3d770bdf8699803a2c1c9f44d48219f09bb28
Alex <sasha393@bk.ru>
2020-05-31 14:08:22 +0300

    fixed errors with static css

--- myshop/urls.py
+++ myshop/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-#from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+#from django.contrib.staticfiles.urls import static
+#from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,5 +29,5 @@ if settings.DEBUG:
     #                document_root=settings.MEDIA_ROOT)
     urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += staticfiles_urlpatterns()
+#urlpatterns += staticfiles_urlpatterns()
 #urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
